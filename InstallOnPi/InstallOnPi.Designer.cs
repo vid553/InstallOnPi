308b6724eef9648a304559221f8b8ab58125a4df
Vid Rajtmajer <vid.rajtmajer@gmail.com>
2018-08-26 21:37:14 +0200

    Initial commit

---
+++ InstallOnPi/InstallOnPi.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace InstallOnPi
+{
+    partial class InstallOnPi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InstallOnPi));
+            this.InstallBtn = new System.Windows.Forms.Button();
+            this.CancelBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // InstallBtn
+            // 
+            this.InstallBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.InstallBtn.BackColor = System.Drawing.Color.SteelBlue;
+            this.InstallBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.InstallBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.InstallBtn.Location = new System.Drawing.Point(12, 266);
+            this.InstallBtn.Name = "InstallBtn";
+            this.InstallBtn.Size = new System.Drawing.Size(151, 26);
+            this.InstallBtn.TabIndex = 0;
+            this.InstallBtn.Text = "Namesti program";
+            this.InstallBtn.UseVisualStyleBackColor = false;
+            this.InstallBtn.Click += new System.EventHandler(this.InstallBtn_Click);
+            // 
+            // CancelBtn
+            // 
+            this.CancelBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.CancelBtn.BackColor = System.Drawing.Color.SteelBlue;
+            this.CancelBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CancelBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.CancelBtn.Location = new System.Drawing.Point(188, 266);
+            this.CancelBtn.Name = "CancelBtn";
+            this.CancelBtn.Size = new System.Drawing.Size(151, 26);
+            this.CancelBtn.TabIndex = 1;
+            this.CancelBtn.Text = "Izhod";
+            this.CancelBtn.UseVisualStyleBackColor = false;
+            this.CancelBtn.Click += new System.EventHandler(this.CancelBtn_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(157, 134);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(138, 24);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox2.Location = new System.Drawing.Point(157, 177);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '●';
+            this.textBox2.Size = new System.Drawing.Size(138, 24);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox3.Location = new System.Drawing.Point(157, 93);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(138, 24);
+            this.textBox3.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.LightSkyBlue;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(332, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Vzpostavitev sistema za pretakanje";
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.AutoSize = true;
+            this.infoLabel.BackColor = System.Drawing.Color.Transparent;
+            this.infoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.infoLabel.ForeColor = System.Drawing.Color.White;
+            this.infoLabel.Location = new System.Drawing.Point(9, 58);
+            this.infoLabel.MinimumSize = new System.Drawing.Size(333, 18);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(333, 18);
+            this.infoLabel.TabIndex = 6;
+            this.infoLabel.Text = "Vnesite podatke za povezavo z napravo:";
+            this.infoLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(79, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 18);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "IP naslov:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(26, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(125, 18);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Uporabniško ime:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(99, 180);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 18);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Geslo:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox4.Location = new System.Drawing.Point(157, 220);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(138, 24);
+            this.textBox4.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(23, 223);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 18);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Različica Pi OS-a:";
+            // 
+            // InstallOnPi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = global::InstallOnPi.Properties.Resources.install_background1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(351, 301);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.infoLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.CancelBtn);
+            this.Controls.Add(this.InstallBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "InstallOnPi";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "InstallOnPi";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.InstallOnPi_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button InstallBtn;
+        private System.Windows.Forms.Button CancelBtn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label infoLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ InstallOnPi/InstallOnPi.cs
@@ -0,0 +1,111 @@
+﻿/*
+ * Install On Pi Application
+ * 
+ * Copyright (C) 2018 Vid Rajtmajer <vid.rajtmajer@gmail.com>
+ * 
+ * Icon made by geotatah from www.flaticon.com
+ * Background photo by Juan from www.pexels.com
+ */
+
+using System;
+using System.Net;
+using System.Security;
+using System.Windows.Forms;
+using Renci.SshNet;
+
+namespace InstallOnPi
+{
+    public partial class InstallOnPi : Form
+    {
+        private SshClient sshClient = null;
+
+        string configCommands = "sudo apt-get install libopus0 libasound2 libudev0 libavahi-client3 libcurl3 libevdev2 ; " +
+                             "echo 'deb http://archive.itimmer.nl/raspbian/moonlight OS main' | sudo tee -a  /etc/apt/sources.list && " +
+                             "wget http://archive.itimmer.nl/itimmer.gpg && " +
+                             "sudo apt-key add itimmer.gpg && " +
+                             "sudo apt-get update && " +
+                             "sudo apt-get install moonlight-embedded";
+
+        public InstallOnPi()
+        {
+            InitializeComponent();
+            textBox4.Text = "jessie";
+        }
+
+        private void InstallBtn_Click(object sender, EventArgs e)   // vzpostavimo povezavo z napravo
+        {
+            infoLabel.Text = "Nalagam ...";
+            try
+            {
+                configCommands = configCommands.Replace("OS", textBox4.Text);
+                string deviceIp = textBox3.Text;
+                string deviceUsername = textBox1.Text;
+                SecureString devicePassword = new NetworkCredential("", textBox2.Text).SecurePassword;
+                if (deviceIp != null && deviceUsername != null && devicePassword != null)
+                {
+                    ConnectionInfo connectionInfo = new ConnectionInfo(deviceIp, deviceUsername, new PasswordAuthenticationMethod(deviceUsername, new NetworkCredential("", devicePassword).Password));
+                    sshClient = new SshClient(connectionInfo);
+                    sshClient.Connect();
+                    if (sshClient.IsConnected == true)
+                    {
+                        RunCommands();
+                    }
+                    else
+                    {
+                        infoLabel.Text = "Napaka! Preverite povezavo z napravo...";
+                        infoLabel.Visible = true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Napaka pri vzpostavitvi povezave z napravo.");
+                infoLabel.Text = "Ni povezave :(";
+                infoLabel.Visible = true;
+                sshClient.Dispose();
+                sshClient = null;
+            }
+        }
+
+        private void RunCommands()  // izvedemo ukaze za namestitev vseh potrebnih programov
+        {
+            infoLabel.Text = "Nameščam programe...";
+            if (configCommands != "")
+            {
+                SshCommand sshCommand = sshClient.CreateCommand(configCommands);
+                string result = sshCommand.Execute();
+                if (result.Contains("error") || result.Contains("ni mogoče"))
+                {
+                    MessageBox.Show("Prišlo je do napake pri nameščanju! Sporočilo napake:\n" + result.ToString());
+                    infoLabel.Text = "Nameščanje ni uspelo!";
+                }
+                else
+                {
+                    infoLabel.Text = "Končano!";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Napaka pri pošiljanju ukazov na napravo!");
+            }
+        }
+
+        private void CancelBtn_Click(object sender, EventArgs e)    // Klik na gumb prekliči
+        {
+            Close();
+        }
+
+        private void InstallOnPi_FormClosing(object sender, FormClosingEventArgs e) // sprostimo vire ko se aplikacija zapre
+        {
+            if (sshClient != null)
+            {
+                if (sshClient.IsConnected == true)
+                {
+                    sshClient.Disconnect();
+                }
+                sshClient.Dispose();
+                this.Dispose();
+            }
+        }
+    }
+}
